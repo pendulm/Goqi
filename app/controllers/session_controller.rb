d330ba90ba131e70134060d3019803d32df727c3
pendulm <lonependulm@gmail.com>
2012-05-02 17:34:33 +0800

    init

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,13 @@
+class SessionController < ApplicationController
+  def index
+  end
+
+  def login
+  end
+
+  def signup
+  end
+
+  def logout
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goqi::Application.config.secret_token = '9114731afe2f2c74150e1a74d291f0bda6d92e7f903e5d39e06e419f3c8619faa66387be611cd45460e7bf9b93e12bc4833fe87d3fd611802b6dbffbd899d6e9'
